c88d94401856b15706d6685788369b8f0eb459ab
Chege-Simon <51915460+Chege-Simon@users.noreply.github.com>
2019-06-18 15:18:37 +0300

    Add files via upload

---
+++ the website/JavaScript 30 projects/Drum Kit/behave.js
@@ -0,0 +1,11 @@
+
+
+// look();
+
+// function look() {
+//     window.addEventListener('keydown', function (e) {
+//         document.querySelector(`[data-key="${e.keyCode}"]`).style.borderColor='#ffc600';
+//         document.querySelector(`[data-key="${e.keyCode}"]`).style.transform = 'scale(1.1)';
+//     document.getElementsByClassName('key').transition="All 0.07s";
+//     });
+// }
---
+++ the website/index.js
@@ -0,0 +1,15 @@
+var button = document.getElementsByTagName("input")[buttonlength -1];
+button.onclick = alert("submitting");
+
+
+// function showPop(){
+//     var name = document.getElementsByTagName("input")[0].value;
+//     alert( name + "Message submited");
+// }
+
+
+
+// var home = document.getElementsByTagName("input")[-1];
+// home.onclick = alert("submitting");
+
+//mfind code to arrange navigation bar when in smaller device
